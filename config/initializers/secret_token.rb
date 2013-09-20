10f2cba4c6e5a9c806f406a90383ffb92387bab8
Florian Kappert <florian@videri-concept.de>
2013-09-20 20:25:07 +0200

    Initial Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offerlist::Application.config.secret_token = '1718122833727a2ef96cadd5f9656bb349640f3ce7beaca8a6ac00bed89b999330e6cc7e7108fb7ef8b2d6ec54c02ccac91e26a6e5e7b4614e355a65c9a82c45'
